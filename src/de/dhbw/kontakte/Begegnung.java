197359a81dcd094a012b53265fb3714219a5a4b8
randomdude433 <jakobthomas2@t-online.de>
2021-03-31 13:04:31 +0200

    Implementierung der Interfaces in die Kontakte.java Erstellung der Klasse Begegnung um der Liste Begegnungen eine Begegnung hinzufügen kann. Erstellung der Klasse Besuch um Besuche hinzufügen zu können

---
+++ src/de/dhbw/kontakte/Begegnung.java
@@ -0,0 +1,11 @@
+package de.dhbw.kontakte;
+
+public class Begegnung {
+
+
+        public Begegnung (Person person1, Person person2) {
+            person1.getId();
+            person2.getId();
+
+        }
+}
---
+++ src/de/dhbw/kontakte/Besuch.java
@@ -0,0 +1,12 @@
+package de.dhbw.kontakte;
+
+
+
+
+public class Besuch {
+    public Besuch (Person person, Ort ort){
+        person.getId();
+        ort.getOrtId();
+
+    }
+}
